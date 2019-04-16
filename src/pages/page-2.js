91bcc9b656c2aec04f9f59b42d3eed92b9b754a4
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-04-16 13:48:11 -0400

    Added Material Ui, gatsby config, and sample pages

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,80 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogActions from '@material-ui/core/DialogActions'
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
+import { Link } from 'gatsby'
+import withRoot from '../withRoot'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+const styles = theme => ({
+  root: {
+    textAlign: 'center',
+    paddingTop: theme.spacing.unit * 20,
+  },
+})
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+class Index extends React.Component {
+  state = {
+    open: false,
+  }
 
-export default SecondPage
+  handleClose = () => {
+    this.setState({
+      open: false,
+    })
+  }
+
+  handleClick = () => {
+    this.setState({
+      open: true,
+    })
+  }
+
+  render() {
+    const { classes } = this.props
+    const { open } = this.state
+
+    return (
+      <div className={classes.root}>
+        <Dialog open={open} onClose={this.handleClose}>
+          <DialogTitle>Super Secret Password</DialogTitle>
+          <DialogContent>
+            <DialogContentText>1-2-3-4-5</DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button color="primary" onClick={this.handleClose}>
+              OK
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <Typography variant="h4" gutterBottom>
+          Material-UI
+        </Typography>
+        <Typography variant="subtitle1" gutterBottom>
+          example project
+        </Typography>
+        <Typography gutterBottom>
+          <Link to="/about">Go to the about page</Link>
+        </Typography>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={this.handleClick}
+        >
+          Super Secret Password
+        </Button>
+      </div>
+    )
+  }
+}
+
+Index.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withRoot(withStyles(styles)(Index))
