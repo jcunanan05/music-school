7e5cd7dc6fac7d53385e2d89eab1d5f9286bd497
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-04-16 14:23:58 -0400

    Add Footer Component
    Also in storybook

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+
+function Footer() {
+  return (
+    <footer>
+      <Typography>Footer</Typography>
+    </footer>
+  )
+}
+
+export default Footer
