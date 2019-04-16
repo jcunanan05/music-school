3d09b4d7261708fea0f41d8475b740cff281ece4
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-04-16 14:05:07 -0400

    Edit Site defaults
    Title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Weston Music School`,
+    description: `Inquiries Welcome, You can learn Music! Children to adults are welcome.`,
+    author: `Jonathan Cunanan <cunanan.jonathan1995@gmail.com>`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -23,8 +23,5 @@ module.exports = {
         // icon: `src/images/gatsby-icon.png`
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
